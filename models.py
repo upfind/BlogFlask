5e591f416e4767b4ee64cd3d45cdcc4b1aa82ffa
ucm <824429779@qq.com>
2018-04-13 09:33:02 +0800

    1.详情页添加评论列表布局

--- models.py
+++ models.py
@@ -27,8 +27,10 @@ class Answer(db.Model):
     __tablename__ = 'answer'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     content = db.Column(db.Text, nullable=False)
+    create_time = db.Column(db.DateTime, default=datetime.now)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    question = db.relationship('Question', backref=db.backref('answers'))
+    question = db.relationship('Question', backref=db.backref('answers',
+                                                              order_by=id.desc()))
     author = db.relationship('User', backref=db.backref('answers'))
