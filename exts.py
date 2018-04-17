3f90de41cfd1dfcb0d6eb8050fff118071434952
ucm <824429779@qq.com>
2018-04-17 08:34:31 +0800

    1.详情页添加评论列表布局

--- exts.py
+++ exts.py
@@ -1,5 +1,10 @@
 # encoding:utf-8
 
 from flask_sqlalchemy import SQLAlchemy
+from  flask_login import LoginManager
+from flask_openid import OpenID
 
 db = SQLAlchemy()
+
+lm = LoginManager
+oid = OpenID()
